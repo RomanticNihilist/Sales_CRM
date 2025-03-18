3df1165617ba0783e170e96a1f5019d924de8d6a
RomanticNihilist <agniva.bhattacharjee@keross.com>
2025-03-18 15:36:34 +0530

    Changed some import in the IOS folder, event_form_ios.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sale_crm/Sales_CRM_android/common_components/custom_appbar/controller/app_bar_controller.dart';
 import 'package:sale_crm/services/api_service.dart';
 import 'package:sale_crm/splash_screen.dart';
-import 'package:sale_crm/splash_screen_ios.dart';
-
-import 'authentication/login.dart';
 import 'authentication/login_ios.dart';
 
 void main() async {
@@ -21,26 +17,30 @@ class SalesCRM extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // if (Platform.isAndroid) {
-    //   return GetMaterialApp(
-    //     debugShowCheckedModeBanner: false,
-    //     title: 'Sales CRM App',
-    //     home: SplashScreen(),
-    //   );
-    // }
-    // else{
-    //   return GetCupertinoApp(
-    //     debugShowCheckedModeBanner: false,
-    //     title: "Sales CRM IOS",
-    //     home: LoginPageIos(),
-    //   );
-    // }
-
-    return GetCupertinoApp(
-      debugShowCheckedModeBanner: false,
-      title: "Sales CRM IOS",
-      home: SplashScreenIos(),
-    );
-
+    if (Platform.isAndroid) {
+      return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Sales CRM App',
+        home: SplashScreen(),
+      );
+      // return GetCupertinoApp(
+      //   debugShowCheckedModeBanner: false,
+      //   title: "Sales CRM IOS",
+      //   home: SplashScreenIos(),
+      // );
+    }
+    else{
+      return GetCupertinoApp(
+        debugShowCheckedModeBanner: false,
+        title: "Sales CRM IOS",
+        home: LoginPageIos(),
+      );
+    }
+    //
+    // return GetCupertinoApp(
+    //   debugShowCheckedModeBanner: false,
+    //   title: "Sales CRM IOS",
+    //   home: LoginPageIos(),
+    // );
   }
 }
